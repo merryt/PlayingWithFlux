71c728761cd37100450f79efa06b5cf1098d0578
Tyler Merry <tymerry@gmail.com>
2015-11-18 08:23:05 -0700

    added some methods to the app store

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import AppActions from  '../actions/app-actions';
 
 export default class App extends React.Component {
   render(){
-    return <h1>Flux App woop</h1>
+    return <h1
+    onClick={AppActions.addItem.bind(null, 'this is an item')}
+    >Flux App woop</h1>
   }
 }
